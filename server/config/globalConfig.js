f6de9b78dca62d83067f79ca44b036ca9cccd2ec
Léo <leo.grivel@epitech.eu>
2017-11-21 15:52:19 +0100

    Forgotten env replaced

--- server/config/globalConfig.js
+++ server/config/globalConfig.js
@@ -2,5 +2,7 @@ module.exports = {
   apiEndpoint: "https://api.intra.42.fr/",
   serverPort: 8080,
   clientPath: "/./public/",
-  refreshRate: 30
+  refreshRate: 30,
+  client_id: "",
+  client_secret: ""
 };
