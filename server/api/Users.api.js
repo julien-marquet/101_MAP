99138d9a625c1aaa7e89b0f3b33e2bd201130993
Julien Marquet <jmarquet@z2r2p6.le-101.fr>
2017-12-05 22:04:47 +0100

    Requested changes : spread operator

--- server/api/Users.api.js
+++ server/api/Users.api.js
@@ -34,14 +34,15 @@ class Users {
                         callback({success: false, message: "can't get server token"});
                     });
                 } else {
-                    self.globalStorage.connected_users_array =  Object.assign(
-                        {}, 
-                        ...usersArray.map((all) => ({ [all.host]: {
-                            begin_at: all.begin_at,
-                            user: all.user,
-                            id: all.id}
+                    self.globalStorage.connected_users_array = {
+                        ...usersArray.map(({begin_at, user, id, host}) => ({
+                            [host]: {
+                                begin_at,
+                                user,
+                                id
+                            }
                         }))
-                      );
+                    };
                     self.globalStorage.connected_users_last_request = Date.now();
                     if (usersArray.length > 0) {
                         callback({
