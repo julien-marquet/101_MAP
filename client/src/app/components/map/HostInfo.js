2b1481d9016e781c9ac366f8345adc625dd23614
Julien Marquet <jmarquet@z2r3p3.le-101.fr>
2017-12-22 16:04:44 +0100

    add css changes

--- client/src/app/components/map/HostInfo.js
+++ client/src/app/components/map/HostInfo.js
@@ -9,25 +9,28 @@ class HostInfo extends Component {
     }
     addDefaultSrc(ev) {
         if(this.props.activeUser !== undefined && ev.target.src.slice(-3) !== 'jpg')
-		    ev.target.src = `https://cdn.intra.42.fr/users/medium_${this.props.activeUser.user.login}.jpg`
+		    ev.target.src = `https://cdn.intra.42.fr/users/large_${this.props.activeUser.user.login}.jpg`
 	}	
 	render() {
 		return (
 				<div className={'hostInfoWrapper'}>
 					<div className={'splitter'}>
 						<div className={'leftCol'}>
-							<img className={'userPortrait'} onError={this.addDefaultSrc} src={`https://cdn.intra.42.fr/users/medium_${this.props.activeUser.user.login}.JPG`}/>
+							<img className={'userPortrait'} onError={this.addDefaultSrc} src={`https://cdn.intra.42.fr/users/large_${this.props.activeUser.user.login}.JPG`}/>
 						</div>
 						<div className={'rightCol'}>
-							<div className={'skewed'}>
-							<div className={'hostName'}>
-								<p>{this.props.activeUser.hostname}</p>
-							</div>
-							</div>
-							<div className={'notskewed'} >
+							<div className={'main skewed'} />
+							<div className={'secondary skewed'} />
+							<div className={'contentTop hostContent'} >
 							<div className={'userName'}>
 								<h2>{this.props.activeUser.user.login}</h2>
 							</div>
+							</div>
+							<div className={'contentBottom hostContent'} >
+							<div className={'hostName'}>
+								<p>{this.props.activeUser.hostname}</p>
+							</div>
+
 							<UpTime begin_at={this.props.activeUser.begin_at} />
 							<a className={'linkUserAccount'} href={"https://profile.intra.42.fr/users/" + this.props.activeUser.user.login}>
 								<span>Profil</span>
