a7f14ad9ccb937f25f2d41b0c988539e1165a048
Leo Grivel <legrivel@z2r3p5.le-101.fr>
2017-12-22 21:20:17 +0100

    Seat animation done

--- client/src/app/components/map/Seat.js
+++ client/src/app/components/map/Seat.js
@@ -4,16 +4,14 @@ class Seat extends Component {
     constructor(props) {
         super(props);
 
-        this.state = {
-            zoomed: false
-        };
-
         this.addDefaultSrc = this.addDefaultSrc.bind(this);
     }
+
     addDefaultSrc(ev) {
         if(this.props.user !== undefined && ev.target.src.slice(-3) != "jpg")
             ev.target.src = `https://cdn.intra.42.fr/users/small_${this.props.user.user.login}.jpg`;
     }
+
     render() {
         if (this.props.user === undefined) {
             return (
@@ -24,20 +22,15 @@ class Seat extends Component {
         }
         else {
             return (
-                <div
-                    className={this.state.zoomed ? "seat" : null}
-                    style={this.state.zoomed ? {position: "relative", overflow: "visible"} : null}
-                >
+                <div className={"seat"}>
                     <div
-                        className={this.state.zoomed ? "seatHover" : "seat"}
-                        onMouseOver={() => this.setState({zoomed: true})}
+                        className={"seatHover"}
                         onClick={() => {
                             this.props.storeActiveUsers({
                                 ...this.props.user,
                                 hostname: this.props.hostname
                             });
                         }}
-                        onMouseOut={() => this.setState({zoomed: false})}
                     >
                         <img
                             onError={this.addDefaultSrc}
