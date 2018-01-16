1165c097b17f8d7ce7fb881ded26d50c351e740e
Julien Marquet <julienmarquet9@gmail.com>
2018-01-16 16:00:05 +0100

    new cross-platform search bar

--- client/src/app/components/map/SearchBar.js
+++ client/src/app/components/map/SearchBar.js
@@ -1,58 +1,61 @@
 import React, {Component} from "react";
+import Autocomplete from "react-autocomplete";
 
 class SearchBar extends Component {
     constructor(props) {
         super(props);
         
         this.handleChange = this.handleChange.bind(this);
-        this.renderDataList = this.renderDataList.bind(this);
+        this.handleSelection = this.handleSelection.bind(this);
         this.results = [];
+        this.focusOut = null;
     }
-    handleChange(event) {
-        this.props.updateSearch(event.target.value);
+    handleChange(event, value) {
+        this.props.updateSearch(value);
         const perfectMatch = this.results.find(elem => elem.user.login === event.target.value);
         if (perfectMatch !== undefined)
             this.props.storeActiveUsers(perfectMatch);
-        
     }
-    renderDataList() {
+    handleSelection(value, item) {
+        this.props.updateSearch(value);
+        this.props.storeActiveUsers(item);
+        this.focusOut.focus();
+    }
+    getMatchingUsers() {
         this.results = [];         
-        if (this.props.users.length !== 0 && this.props.searchedUser.length > 0) {
-            Object.entries(this.props.users).forEach(value => {
-                if (value[1].user.login.includes(this.props.searchedUser.toLowerCase()))
-                    this.results.push({
-                        ...value[1],
-                        hostname: value[0]
-                    });
-            });
-            return (
-                <datalist id={"searchUsers"}>
-                    {
-                        this.results.map((user, index) => {
-                            return (
-                                <option key={`searchResult-${index}`} value={user.user.login} className={"searchResultNode"} />
-                            );
-                        })
-                    }
-                </datalist>
-            );
-        }
+        Object.entries(this.props.users).forEach(value => {
+            if (value[1].user.login.includes(this.props.searchedUser.toLowerCase()))
+                this.results.push({
+                    ...value[1],
+                    hostname: value[0]
+                });
+        });
+        return this.results;
     }
     render() {
-        return (
-            <div className={"searchBar"}>
-                <input
-                    type={"text"}
-                    list={"searchUsers"}
-                    key={"searchInput"}
-                    type={"text"}
-                    className={"searchbar"}
-                    placeholder={"Search..."}
-                    onChange={this.handleChange}
-                    value={this.props.searchedUser}
 
+        return (
+            <div className={"searchBar"} ref={(item) => this.focusOut = item}>
+                <Autocomplete
+                    wrapperProps={{
+                        className: "wrapperSearch",
+                        style:{}
+                    }}
+                    renderMenu={(items) => {
+                        return <div className="searchResults" children={items}/>;
+                    }}
+                    open={this.props.searchedUser.length > 2}
+                    getItemValue={(item) => item.user.login}
+                    items={this.getMatchingUsers()}
+                    renderItem={(item, isHighlighted) =>
+                        <div key={`item${item.id}`} style={{ background: isHighlighted ? "lightgray" : "white" }}>
+                            {item.user.login}
+                        </div>
+                    }
+                    value={this.props.searchedUser}
+                    onChange={this.handleChange}
+                    onSelect={this.handleSelection}
                 />
-                {this.renderDataList()}
             </div>
         );
     }
