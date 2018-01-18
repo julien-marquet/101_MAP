865735804ac4749c69df9f3ace5121f18a5eccb3
Julien Marquet <jmarquet@z2r1p2.le-101.fr>
2018-01-18 20:53:13 +0100

    logger for user.get.infos

--- server/websocket_event/users.websocket.js
+++ server/websocket_event/users.websocket.js
@@ -1,13 +1,20 @@
-const Users = require("../api/Users.api");
-const authenticator = require('../OAuth2_authenticator');
+const   Users = require("../api/Users.api"),
+        authenticator = require("../OAuth2_authenticator"),
+        logger = require("../logger");
 
 const usersSocket = (socket, globalStorage) => {
     const auth = new authenticator(globalStorage);
     const UsersModel = new Users(globalStorage, auth);
     socket.on("user.get.infos", ({userId, userToken}) => {
         UsersModel.getUserInfos(userId, userToken)
-            .then(response => socket.emit("user.getted.infos", response))
-            .catch(error => socket.emit("error.fetch", error));
+            .then(response => {
+                logger.add_log("General", "Request UserInfos Succeeded");
+                socket.emit("user.getted.infos", response)
+            })
+            .catch(error => {
+                logger.add_log("Error", `Request UserInfos Failed. details : ${JSON.stringify(error)}`);                
+                socket.emit("error.fetch", error)
+        });
     });
 };
 
