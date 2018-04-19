4a822cbb72128aa460642925073e34333b2db967
Leo Grivel <legrivel@student.le-101.fr>
2018-04-19 17:59:24 +0200

    Centralized events

--- client/src/app/components/sidebar/SideBar.js
+++ client/src/app/components/sidebar/SideBar.js
@@ -5,27 +5,6 @@ import DisconnectSwitch from "../../containers/sidebar/disconnectSwitch";
 import PassiveModeSwitch from "../../containers/sidebar/passiveModeSwitch";
 
 class SideBar extends Component {
-    constructor(props) {
-        super(props);
-
-        this.keyDown = this.keyDown.bind(this);
-    }
-
-    componentWillMount() {
-        document.body.addEventListener("keydown", this.keyDown);
-    }
-
-    componentWillUnmount() {
-        document.body.removeEventListener("keydown", this.keyDown);
-    }
-
-    keyDown({keyCode}) {
-        if ((keyCode === 27 || keyCode === 70) && this.props.mode === "passive") {
-            this.props.quitPassiveMode();
-            localStorage.removeItem("mode");
-        }
-    }
-
     render() {
         return (
             <div className={this.props.mode === "passive" ? "sidebar sidebarHided" : "sidebar"}>
