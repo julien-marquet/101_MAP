b4a2d06fbaebea50e80da81a12a7e94042fdb83e
Leo Grivel <legrivel@student.le-101.fr>
2018-05-15 18:19:40 +0200

    Removed clientId

--- client/src/config/globalConfig.js
+++ client/src/config/globalConfig.js
@@ -1,6 +1,6 @@
 export default {
     serverEndpoint: "http://localhost:8080",
-    clientId: "6a70908edc7b210ad935bfeb58ec10870f4f5c9ed4072356aa4a97ea3ff42bf8",
+    clientId: "7e72ed3b0dfd8a5499a8337e11fcd3e64467b975c0f4fabaf4bc334d358c627b",
     redirectUri: "http://localhost:3000/",
     apiEndPoint: "https://api.intra.42.fr",
     defaultTheme: "dark",
