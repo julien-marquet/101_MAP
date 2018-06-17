5d2d0707c09caedde75de83b3a398d039267dafa
Julien Marquet <julienmarquet9@gmail.com>
2018-06-17 23:15:05 +0200

    Client going forward

--- client/src/app/components/Scorer.js
+++ client/src/app/components/Scorer.js
@@ -1,5 +1,6 @@
 import React, {Component} from "react";
 import "../scss/scorer.css";
+import placeholder from "../../img/placeholder.png";
 
 class Scorer extends Component {
 
@@ -8,7 +9,7 @@ class Scorer extends Component {
 
         this.state = {
             dismissed: false
-        }
+        };
     }
 
     render(){ 
@@ -25,7 +26,30 @@ class Scorer extends Component {
                     }}>
                         <i className="fas fa-times" />
                     </button>
-                
+                    <div className={"concurrentWrapper"}>
+                        <div className={"concurrentBlock win"}>
+                            <img src={placeholder} />
+                            <p className={"concurrentLogin"}>Jean-patrick</p>
+                            <p className={"scoreLabel"}>Score :</p>
+                            <div className={"scoreBlock"}>
+                                101
+                            </div>
+                            <div className={"concurrentStatus"}>
+                                <p>Win</p>
+                            </div>
+                        </div>
+                        <div className={"concurrentBlock loose"}>
+                            <img src={placeholder} />
+                            <p className={"concurrentLogin"}>Kimberlay</p>
+                            <p className={"scoreLabel"}>Score :</p>
+                            <div className={"scoreBlock"}>
+                                42
+                            </div>
+                            <div className={"concurrentStatus"}>
+                                <p>Loose</p>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
         );
