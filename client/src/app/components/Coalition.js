256e3b8521d9120d1ca78710f098bedb224152b2
legrivel <legrivel@student.le-101.fr>
2018-11-21 14:10:03 +0100

    Better design

--- client/src/app/components/Coalition.js
+++ client/src/app/components/Coalition.js
@@ -1,4 +1,4 @@
-import React, {Component} from "react";
+import React, { Component } from "react";
 import PropTypes from "prop-types";
 
 class Coalitions extends Component {
@@ -9,10 +9,12 @@ class Coalitions extends Component {
     render() {
         return (
             <div className={"coalitionScore"}>
-                <div className={"logoBackground"}  style={{backgroundColor: this.props.color}}>
-                    <img src={this.props.image_url} style={{marginTop: this.props.name.toLowerCase() === "ranger" ? "-15%" : "0"}}/>
+                <div className={"logoBackground"} style={{ backgroundColor: this.props.color }}>
+                    <div>
+                        <img src={this.props.image_url} style={{ marginTop: this.props.name.toLowerCase() === "ranger" ? "-12%" : "0" }} />
+                    </div>
                 </div>
-                <p style={{color: this.props.color}}>{this.props.score}</p>
+                <p style={{color: this.props.color }}>{this.props.score}</p>
             </div>
         );
     }
