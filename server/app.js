694ce25cfb25f62e1c941c426adf61e72e32a8cb
legrivel <legrivel@student.le-101.fr>
2019-01-29 18:53:36 +0100

    Getting env path

--- server/app.js
+++ server/app.js
@@ -1,4 +1,4 @@
-require("dotenv").config();
+require("dotenv").config({path: `${__dirname}/.env`});
 const express = require("express"), 
     databaseConfig = require("./config/databaseConfig"),
     mongoose = require("mongoose"),
