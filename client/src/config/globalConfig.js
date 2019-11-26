a3662247b0373420ce257c74fcf48789c580abde
root <root@The-Matrix.le-101.fr>
2019-11-26 20:55:15 +0100

    Update with prod configs

--- client/src/config/globalConfig.js
+++ client/src/config/globalConfig.js
@@ -1,7 +1,7 @@
 export default {
-    serverEndpoint: "http://localhost:8080",
-    clientId: "6a70908edc7b210ad935bfeb58ec10870f4f5c9ed4072356aa4a97ea3ff42bf8",
-    redirectUri: "http://localhost:3000/",
+    serverEndpoint: "https://the-matrix.le-101.fr",
+    clientId: "5f6825337eba76aaf3f707d27c570493a3b14b2d52e33a6f058312c15defcc95",
+    redirectUri: "https://the-matrix.le-101.fr/",
     apiEndPoint: "https://api.intra.42.fr",
     defaultTheme: "dark",
     minimalSearchInput: 2,
@@ -9,5 +9,6 @@ export default {
         "dark",
         "light"
     ],
-    konamiUrl: "http://localhost:3000/"
+    konamiUrl: "https://the-matrix.le-101.fr/memes"
 };
+
--- server/config/globalConfig.js
+++ server/config/globalConfig.js
@@ -1,11 +1,11 @@
 module.exports = {
-    apiEndpoint: "https://api.intra.42.fr/",
-    serverPort: 8080,
-    clientPath: "/../public/",
-    connectedUsers_loopRate: 30, // seconds
-    requestsBySecond: 2, // In queue
-    connectedUsers_cacheExpiration: 10, // minutes
-    redirect_uri: "http://localhost:3000/",
-    streamLogToConsole: true,
-    blocId: 8
+  apiEndpoint: "https://api.intra.42.fr/",
+  serverPort: 8080,
+  clientPath: "/../public/",
+  connectedUsers_loopRate: 30, // seconds
+  requestsBySecond: 4, // In queue
+  connectedUsers_cacheExpiration: 10, // minutes
+  redirect_uri: "https://the-matrix.le-101.fr/",
+  streamLogToConsole: true,
+  blocId: 8
 };
