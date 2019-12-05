d574713ea6d16d34ed03a5530235d9d9035bc408
Manuel Moya <mmoya@z2r1p1.le-101.fr>
2019-12-05 22:38:09 +0100

    Updated search style

--- client/src/app/components/map/Seat.js
+++ client/src/app/components/map/Seat.js
@@ -44,8 +44,8 @@ class Seat extends Component {
         }
         else if (nextProps.user !== undefined &&
             nextProps.searchedUser.length >= globalConfig.minimalSearchInput &&
-            ((nextProps.user.login.includes(nextProps.searchedUser.toLowerCase()) && !this.state.isSearched) ||
-            (!nextProps.user.login.includes(nextProps.searchedUser.toLowerCase()) && this.state.isSearched))) {
+            ((nextProps.user.login.includes(nextProps.searchedUser.toLowerCase()) && this.state.isSearched) ||
+            (!nextProps.user.login.includes(nextProps.searchedUser.toLowerCase()) && !this.state.isSearched))) {
             this.setState({isSearched: !this.state.isSearched});
         }
         if (nextProps.activeUser.id === 0 && this.state.isActive) {
@@ -92,9 +92,12 @@ class Seat extends Component {
         }
         else {
             let className = "seatHover";
-            if (this.state.isSearched || this.state.isActive) {
-                className += " highlighted";
+            if (this.state.isSearched) {
+                className += " hidden";
             }
+			if (this.state.isActive) {
+				className += " highlighted";
+			}
             if (this.props.user.pool) {
                 className += " newbie";
             }
